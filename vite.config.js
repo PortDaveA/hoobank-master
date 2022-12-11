b25ea7c42622c755e141395b2518a33d93c07c52
Croesus <haydave47@gmail.com>
2022-12-11 12:56:27 +0500

    vite.config.js base changed to hoobank-master

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hoobank/', // Url of HooBank's Website
+  base: '/hoobank-master/', // Url of HooBank's Website
   plugins: [react()]
 })
